49687413e25ecf82a3940141f75e281613bd5468
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2026-01-04 00:56:32 +0700

    add static route to static folder\nadd some pictures to static/images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,10 +3,10 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 	"github.com/bangn/bookings/pkg/config"
 	"github.com/bangn/bookings/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -20,6 +20,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
