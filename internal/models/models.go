eeed7e64df58870b45f174bb4c7d73535ba72e49
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2026-03-08 19:48:51 +0700

    Add Model structs to the table of the booking DB

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,8 +1,62 @@
 package models
 
+import "time"
+
+// Reservation is the type for reservations in the system
 type Reservation struct {
+	ID		int
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	Room      Room
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// User is the type for users of the system
+type User struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	created_at  time.Time
+	updated_at  time.Time
+}
+
+// Room is the type for rooms in the system
+type Room struct {
+	ID        int
+	RoomName  string
+	created_at time.Time
+	updated_at time.Time
+}
+
+// Restriction is the type for restrictions in the system
+type Restriction struct {
+	ID        int
+	RestrictionName  string
+	created_at time.Time
+	updated_at time.Time
+}
+
+// RoomRestriction is the type for room restrictions in the system
+type RoomRestriction struct {
+	ID            int
+	RoomID        int
+	RestrictionID int
+	ReservationID int
+	StartDate time.Time
+	EndDate   time.Time
+	Room	Room
+	Reservations Reservation
+	Restrictions Restriction
+
+	created_at    time.Time
+	updated_at    time.Time
 }
